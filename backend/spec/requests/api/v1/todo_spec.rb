d625ea8a9eb0b5ba0d435481c5bbd762c2c05d7a
taji <taji@sonicgarden.jp>
2026-01-12 15:33:38 +0900

    spec追加

--- backend/spec/requests/api/v1/todo_spec.rb
+++ backend/spec/requests/api/v1/todo_spec.rb
@@ -3,45 +3,48 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Todos', type: :request do
   describe 'GET /api/v1/todos' do
     before do
-      create(:todo , content: 'サンプルTODO1')
-      create(:todo , content: 'サンプルTODO2')
-      create(:todo , content: 'サンプルTODO3')
+      create_list(:todo, 3)
     end
     it 'TODOリストを取得する' do
       get '/api/v1/todos'
       
       expect(response).to have_http_status(:success)
+      json = JSON.parse(response.body)
+
+      expect(json['data'].length).to eq(3)
     end
   end
 
   describe 'GET /api/v1/todos/:id' do
+    let!(:todo) { create(:todo, content: '猫買う') }
     it '特定のTODOを取得する' do
-      # todo = Todo.create!(title: 'サンプルTODO')
-      # get "/api/v1/todos/#{todo.id}"
-      # expect(response).to have_http_status(:success)
+      get "/api/v1/todos/#{todo.id}"
+      expect(response).to have_http_status(:success)
+      json = JSON.parse(response.body)
+      expect(json['data']['id'].to_i).to eq(todo.id)
     end
   end
 
   describe 'POST /api/v1/todos' do
     it 'TODOを作成する' do
-      # post '/api/v1/todos', params: { todo: { title: '新しいTODO' } }
-      # expect(response).to have_http_status(:created)
+      post '/api/v1/todos', params: { todo: { content: '新しいTODO' } }
+      expect(response).to have_http_status(:created)
     end
   end
 
   describe 'PUT /api/v1/todos/:id' do
+    let(:todo) { create(:todo, content: '古いTODO') }
     it 'TODOを更新する' do
-      # todo = Todo.create!(title: 'サンプルTODO')
-      # put "/api/v1/todos/#{todo.id}", params: { todo: { title: '更新されたTODO' } }
-      # expect(response).to have_http_status(:success)
+      put "/api/v1/todos/#{todo.id}", params: { todo: { content: '更新されたTODO' } }
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe 'DELETE /api/v1/todos/:id' do
+    let(:todo) { create(:todo, content: '削除するTODO') }
     it 'TODOを削除する' do
-      # todo = Todo.create!(title: 'サンプルTODO')
-      # delete "/api/v1/todos/#{todo.id}"
-      # expect(response).to have_http_status(:no_content)
+      delete "/api/v1/todos/#{todo.id}"
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
